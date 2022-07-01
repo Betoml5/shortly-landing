d0d2ab5667c7811e340576517ce758db7e0f4d7c
Betoml5 <betoml5@hotmail.com>
2022-07-01 00:16:48 -0500

    Add validaton add styles when error

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const $btnMenu = document.querySelector("#btnMenu");
 const $menu = document.querySelector("#menu");
 const $imgWorking = document.querySelector("#imgWorking");
+const $linkInput = document.querySelector("#linkInput");
+const $btnShorten = document.querySelector("#btnShorten");
+const $errorMsg = document.querySelector("#error");
+const $linksContainer = document.querySelector("#links");
+
+const API_URL = "https://api.shrtco.de/v2";
+
 $btnMenu.addEventListener("click", () => {
   if ($menu.style.display === "none") {
     $menu.style.display = "block";
@@ -15,3 +22,52 @@ $btnMenu.addEventListener("click", () => {
     $imgWorking.style.display = "none";
   }
 });
+
+$btnShorten.addEventListener("click", async () => {
+  try {
+    $errorMsg.textContent = "";
+    if ($linkInput.value === "") {
+      $errorMsg.textContent = "Please enter a valid URL";
+      $errorMsg.style.display = "block";
+      $linkInput.style.outline = "2px solid red";
+      $linkInput.classList.add("placeholderRed");
+      return;
+    }
+    $linkInput.classList.remove("placeholderRed");
+    const shortenUrl = await shorten($linkInput.value);
+    $linkInput.style.outline = "inherit";
+    const html = `
+  <div class="bg-white rounded-lg my-10">
+  <p class="text-xl p-4">${$linkInput.value}</p>
+  <div class="h-px bg-[#9e9aa7] opacity-40"></div>
+  <div class="p-4">
+    <p class="text-[#2acfcf] text-xl">${shortenUrl}</p>
+    <button
+      class="bg-[#2acfcf] w-full text-white font-bold px-10 py-3 rounded-lg mt-4"
+      id="btnShorten"
+    >
+      Copy
+    </button>
+  </div>
+</div>
+  `;
+    $linkInput.value = "";
+    $linksContainer.insertAdjacentHTML("beforeend", html);
+  } catch (error) {
+    throw error;
+  }
+});
+
+const shorten = async (url) => {
+  try {
+    const response = await fetch(`${API_URL}/shorten?url=${url}`, {
+      method: "POST",
+    });
+
+    const data = await response.json();
+    console.log(data);
+    return data.result.short_link;
+  } catch (error) {
+    throw error;
+  }
+};
